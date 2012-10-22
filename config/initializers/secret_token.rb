7068e160c1f8d7214b23edbbdf8101fbb0878924
Jessie Keck <jessie.keck@gmail.com>
2012-10-22 10:47:36 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StlpNewsletter::Application.config.secret_token = 'bbbebf7c06df44924e91f416c925cb7084e456341fd7c83512492a78831b43a02d279767ad1316f951962cc120f03f73f443cc6bfe01173444b931ada6a83a50'
