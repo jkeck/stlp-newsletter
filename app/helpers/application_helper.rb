3edd285a804c0fbc72ba119460f2f8b941a79318
Jessie Keck <jessie.keck@gmail.com>
2012-10-22 19:50:35 -0700

    Add quotes to Newsletter UI and add support for custom newsletter layouts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     link_to text, path, :role=>"button", :class=>"btn", :"data-toggle"=>"modal", :"data-modal-id" => id
   end
   
+  def render_newsletter
+    begin
+      render :partial => "newsletters/custom/stlp_#{@newsletter.year}_#{@newsletter.session}"
+    rescue ActionView::MissingTemplate
+      render "newsletters/custom/default"
+    end
+  end
+  
 end
